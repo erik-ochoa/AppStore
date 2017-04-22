61f69db1888648396ee2b1523e8bbb9caa05060d
Erik Ochoa <erikochoa26051@gmail.com>
2017-04-22 14:16:24 -0400

    Names from database
    Names and links of categories are retrieved from databsed in the back
    end

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 '''
 Created on Feb 16, 2017
 
-@author: Kyle
+@author: Kyle and Erik
 '''
 
 from flask import Flask, render_template, request, session
@@ -26,8 +26,8 @@ toolbar = DebugToolbarExtension(app)
 
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jack8998'
-app.config['MYSQL_DATABASE_DB'] = 'db_appstore'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1998compsci'
+app.config['MYSQL_DATABASE_DB'] = 'appstore_db'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
@@ -35,12 +35,63 @@ mysql.init_app(app)
 
 @app.route('/')
 def main():
-    return render_template("index.html")
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    
+    #This gets length of the categories table
+    #table_len = cursor.execute("SELECT ID FROM categories ORDER BY id DESC LIMIT 1")
+   
+    #Assigning every category to an index c_list and image link to img_link
+    c_list = []
+    img_list = []
+    i = 0
+    while i < 3:
+        cursor.execute("SELECT name FROM categories WHERE id=%s",(i))
+        c_list.append(cursor.fetchone()[0])
+        
+        cursor.execute("SELECT image_link FROM categories WHERE id=%s",(i))
+        img_list.append(cursor.fetchone()[0])
+        print(img_list[i])
+        i = i+1
+    
+    return render_template("index.html", c0 = c_list[0], c1 = c_list[1], c2 = c_list[2], i0 = img_list[0], i1 = img_list[1], i2 = img_list[2])
+
+@app.route('/applist/', methods = ['POST', 'GET'])
+def applist():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    
+    #This gets length of the categories table
+    table_len = cursor.execute("SELECT ID FROM categories ORDER BY id DESC LIMIT 1")
+    print (table_len)
+    
+    #This gets all the categories from the database
+    cursor.execute("SELECT * FROM categories")
+    
+    #Assigning every category to a variable (e.g C1 stands for category 1)
+    categories_list = []
+    while i < table_len:
+        categories_list[i]=cursor.fetchone()[i]
+        print(categories_list[i])
+
+    if request.method == 'POST':
+        #appName = request.form['appName']
+        return render_template("applist.html")
+
+
+
+
+
+
 
 # @app.route('/home/')
 # def home():
 #     appName = request.form['InputBox']
 #     return render_template("home.html")
+
+
+
+
 
 @app.route('/home/', methods = ['POST', 'GET'])
 def home():
@@ -134,36 +185,6 @@ def createapp():
 
 
     return render_template("createapp.html", name=name, appname=appname, num_apps=num_apps)
-
-@app.route('/applist/', methods = ['POST', 'GET'])
-def applist():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    #This gets the appStore name which displays at the top
-    cursor.execute("SELECT name FROM appstore_list ORDER BY id DESC LIMIT 1")
-    appstore_name=cursor.fetchone()[0]
-    #This gets the appStore ID which is hidden
-    cursor.execute("SELECT id FROM appstore_list ORDER BY id DESC LIMIT 1")
-    appstore_id=cursor.fetchone()[0]
-
-    if request.method == 'POST':
-        appName = request.form['appName']
-        userReviews = request.form['reviews']
-        downloadCount = request.form['downloadCount']
-        verifiedDeveloper = request.form['verifiedDeveloper']
-        paidApp = request.form['paid']
-        rating = 3.5
-
-        cursor.execute("INSERT INTO app_list (appstore_id, app_name, app_reviews, app_rating, app_download, app_verified, app_paid) VALUES (%s, %s, %s, %s, %s, %s, %s)",(appstore_id, appName, userReviews, rating, downloadCount, verifiedDeveloper, paidApp))
-        conn.commit()
-
-    cursor.execute("SELECT app_download FROM app_list ORDER BY id DESC LIMIT 1")
-    download=cursor.fetchone()[0]
-
-    #Number of apps
-    num_apps=cursor.execute("SELECT appstore_id FROM app_list WHERE appstore_id=%s",(appstore_id))
-
-    return render_template("applist.html", name=appstore_name, appstore_id=appstore_id, download=download, num_apps=num_apps)
 
 @app.route('/applistFromEdit/', methods = ['POST', 'GET'])
 def applistFromEdit():
